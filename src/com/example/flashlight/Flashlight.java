283bfb9a81d7ac3b0d0b42201874f6972deb070e
yaohq <yaohq1988@163.com>
2015-02-03 23:27:12 +0800

    problem: touch one button, it works, then touch another one, it broken.

--- src/com/example/flashlight/Flashlight.java
+++ src/com/example/flashlight/Flashlight.java
@@ -1,6 +1,7 @@
 package com.example.flashlight;
 
 import android.app.Activity;
+import android.content.Context;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.os.Bundle;
@@ -10,13 +11,29 @@ import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.ToggleButton;
 
 public class Flashlight extends Activity {
-	private ToggleButton mButton;
+	private ToggleButton mLightButton;
+	private ToggleButton mFlashButton;
 	private Camera mCamera;
 	private Parameters mParams;
 	
 	public Flashlight(){
-		//constuctor
+		//Constructor
 		//System.out.println("Flashlight start....");
+	}
+	
+	private void OpenLight(){
+		mParams.setFlashMode(Parameters.FLASH_MODE_TORCH);
+		mCamera.setParameters(mParams);
+		mCamera.startPreview();
+//		System.out.println("open light...");
+	}
+	
+	private void CloseLight(){
+		mParams.setFlashMode(Parameters.FLASH_MODE_OFF);
+		mCamera.setParameters(mParams);
+		mCamera.stopPreview();
+//		mCamera.release();
+//		System.out.println("close light...");
 	}
 	
 	@Override
@@ -24,7 +41,11 @@ public class Flashlight extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_flashlight);
 		
-		mButton = (ToggleButton)findViewById(R.id.open_close);
+		mLightButton = (ToggleButton)findViewById(R.id.light);
+		mFlashButton = (ToggleButton)findViewById(R.id.flash);
+		//设置打开应用时的默认显示
+		mLightButton.setText(R.string.open_light);
+		mFlashButton.setText(R.string.open_flash);
 		
 		try{
 			mCamera = Camera.open();
@@ -39,25 +60,53 @@ public class Flashlight extends Activity {
 		
 		mParams = mCamera.getParameters();
 		
-		//保持屏幕常亮
-		getWindow().addFlags(WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON);
-		
-		mButton.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+		//保持屏幕常亮，5分钟自动关闭
+//		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+	
+		mLightButton.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 			@Override
 			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				//打开开关按钮
 				if(isChecked){
-					mParams.setFlashMode(Parameters.FLASH_MODE_TORCH);
-					mCamera.setParameters(mParams);
-					mCamera.startPreview();
+//					mFlashButton.setEnabled(false);
+					OpenLight();
 				}else{
-					mParams.setFlashMode(Parameters.FLASH_MODE_OFF);
-					mCamera.stopPreview();
+					CloseLight();
 					mCamera.release();
+//					mFlashButton.setEnabled(true);
 				}
-				//打开或者关闭闪光灯
+			}
+		});
+		
+		//闪烁10次后自动停止
+		mFlashButton.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//				mFlashButton.setEnabled(false);
+//				mLightButton.setEnabled(false);
+				//如果开关已打开
+				if(isChecked){
+					int i = 0;
+					while(i++ < 10){
+						try {
+							Thread.sleep(200);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+						OpenLight();
+						try {
+							Thread.sleep(200);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+						CloseLight();
+					}
+					mCamera.release();
+					mFlashButton.setChecked(false);
+				}
+				
+//				mFlashButton.setEnabled(true);
+//				mLightButton.setEnabled(true);
 			}
 		});
 	}
-
 }
