069ae8261e8746534d3426e264fdf27fe4193982
yaohq <yaohq1988@163.com>
2015-02-08 14:54:04 +0800

    v1.2

--- src/com/example/flashlight/Flashlight.java
+++ src/com/example/flashlight/Flashlight.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class Flashlight extends Activity {
@@ -65,21 +66,28 @@ public class Flashlight extends Activity {
 		mFlashButton.setText(R.string.open_flash);
 		
 		//保持屏幕常亮，5分钟自动关闭
-//		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 	
 		mLightButton.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 			@Override
 			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 				if(isChecked){
-//					mFlashButton.setEnabled(false);
-					//每次使用闪光灯之前，都要先获取资源
-					getCamera();
-					OpenLight();
+					if(mCamera != null){
+						Toast.makeText(Flashlight.this, R.string.light_open_toast, Toast.LENGTH_SHORT).show();
+					}else{
+						//打开手电时，使打开闪光按钮不可用，否则就会抢夺闪光灯资源
+						mFlashButton.setEnabled(false);
+						//每次使用闪光灯之前，都要先获取资源
+						getCamera();
+						OpenLight();
+					}
 				}else{
 					CloseLight();
 					//每次关闭闪光灯后，都要释放资源
 					mCamera.release();
-//					mFlashButton.setEnabled(true);
+					mCamera = null;
+					//关闭手电时，使闪光按钮可用
+					mFlashButton.setEnabled(true);
 				}
 			}
 		});
@@ -88,35 +96,55 @@ public class Flashlight extends Activity {
 		mFlashButton.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 			@Override
 			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//				mFlashButton.setEnabled(false);
-//				mLightButton.setEnabled(false);
-				//如果开关已打开
+				//如果开关被打开
 				if(isChecked){
-					//每次使用闪光灯之前，都要先获取资源
-					getCamera();
-					int i = 0;
-					while(i++ < 10){
-						try {
-							Thread.sleep(200);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
+					//如果闪光灯正在被使用，不执行闪光功能，提示“闪光灯正在被使用"
+					if(mCamera != null){
+						Toast.makeText(Flashlight.this, R.string.light_open_toast, Toast.LENGTH_SHORT).show();
+					}else{
+						//每次使用闪光灯之前，都要先获取资源
+						getCamera();
+						//获取闪光灯资源后，要使打开手电和打开闪光两个按钮都不可用，否则本次闪光结束后，会继续执行刚才的点击动作
+						mLightButton.setEnabled(false);
+						mFlashButton.setEnabled(false);
+						
+						for(int i = 0; i < 10; i++){
+							try {
+								Thread.sleep(200);
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+							OpenLight();
+							try {
+								Thread.sleep(200);
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+							CloseLight();
 						}
-						OpenLight();
-						try {
-							Thread.sleep(200);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-						CloseLight();
+						//关闭闪光后，释放资源
+						mCamera.release();
+						mCamera = null;
+						//打开闪光按钮调整为关闭状态
+						mFlashButton.setChecked(false);
+						//闪光结束后，使两个按钮恢复可用
+						mLightButton.setEnabled(true);
+						mFlashButton.setEnabled(true);
 					}
-					//每次关闭闪光灯后，都要释放资源
-					mCamera.release();
-					mFlashButton.setChecked(false);
 				}
-				
-//				mFlashButton.setEnabled(true);
-//				mLightButton.setEnabled(true);
 			}
 		});
+	}
+	
+	//Activity销毁时
+	@Override
+	protected void onDestroy() {
+		//如果闪光灯正打开，就关闭闪光灯，释放资源
+		if(mCamera != null){
+			CloseLight();
+			mCamera.release();
+			mCamera = null;
+		}
+		super.onDestroy();
 	}
 }
